1cab83830737bb396813bcdc45a8ec873697e1c6
Leah Nelson and Tony Fabery <leah+tony@>
2016-09-21 09:01:53 -0700

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit :username, :email, :password, :password_confirmation
+    end
+  end
 end
